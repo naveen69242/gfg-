80136ee91fd17cf24fdb643e558ec132ac11b721
Naveen <naveengoyal1416@gmail.com>
2026-08-27 10:45:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Binary String/check-for-binary-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool isBinary(string& s) {
+        // code here
+        for(int i=0;i<s.length();i++){
+            if(s[i]!='0'&&s[i]!='1'){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+};
