7118d9f3f4e609a7c1931b4d1870c8bca03948ae
Naveen <naveengoyal1416@gmail.com>
2026-09-13 18:14:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Odd or Even/odd-or-even.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool isEven(int n) {
+        // code here
+       if(n&1){
+           return false;
+       }else{
+           return true;
+       }
+        
+      
+       
+    }
+};
